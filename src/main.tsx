41e56a2e2a771d5b6e0f651d8278f1496cce0dab
Microck <contact@micr.dev>
2026-05-30 19:31:31 +0000

    docs: sync gamebanana website links

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const preloaderLogoMarkup = preloaderLogoSvg
   .replace(/<!DOCTYPE[^>]*>\s*/u, "");
 
 const gamebananaModId = "681169";
-const gamebananaFallbackFileId = "1714822";
+const gamebananaFallbackFileId = "1715650";
 const gamebananaModUrl = `https://gamebanana.com/mods/${gamebananaModId}`;
 const gamebananaFilesApiUrl =
   `https://api.gamebanana.com/Core/Item/Data?itemtype=Mod&itemid=${gamebananaModId}` +
