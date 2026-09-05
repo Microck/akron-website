50469d25330de5e9eda9f0668d1d9606b5e6b32c
Microck <contact@micr.dev>
2026-09-05 09:41:04 +0000

    docs: sync gamebanana website links

--- api/_gamebanana.js
+++ api/_gamebanana.js
@@ -1,5 +1,5 @@
 const gamebananaModId = "681169";
-const gamebananaFallbackFileId = "1806025";
+const gamebananaFallbackFileId = "1807718";
 const gamebananaFilesApiUrl =
   `https://api.gamebanana.com/Core/Item/Data?itemtype=Mod&itemid=${gamebananaModId}` +
   "&fields=Files().aFiles()&return_keys=1&format=json_min&flags=JSON_UNESCAPED_SLASHES";
